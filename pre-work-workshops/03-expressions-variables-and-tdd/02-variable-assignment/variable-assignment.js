9c04927919417ed925a23179dae528aeb6f39acc
aa-r-ju <aarju3447@gmail.com>
2025-04-22 16:37:21 +0545

    feat: completed 03-expression-variables-and-tdd

--- pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+let myFirstName = "Siya";
+let myFavoriteNum = 8;
+let havingFun = true;
+console.log(myFirstName);
+console.log(myFavoriteNum);
+console.log(havingFun);
--- pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
@@ -1,3 +1,7 @@
 let happyNum = 1000;
 
 // YOUR CODE BELOW
+console.log((happyNum /= 10));
+happyNum += 10;
+happyNum -= 105;
+console.log(happyNum);
