2ad3bd5f9ed54380ffccf985c38dd37f05f3cbed
aa-r-ju <aarju3447@gmail.com>
2025-04-22 15:43:45 +0545

    feat: completed 01 overview-and-setup

--- pre-work-workshops/01-overview-and-setup/01-hello-world/hello-world.js
+++ pre-work-workshops/01-overview-and-setup/01-hello-world/hello-world.js
@@ -1,4 +1,4 @@
-console.log('hello world!');
+console.log("hello world!");
 
 // this is a comment!
 
@@ -6,3 +6,5 @@ console.log('hello world!');
   this is also a comment!
   it spans multiple lines!
 */
+let name = "Aarju";
+console.log("Hello world " + "this is " + name);
--- pre-work-workshops/01-overview-and-setup/02-simple-sum/simple-sum.js
+++ pre-work-workshops/01-overview-and-setup/02-simple-sum/simple-sum.js
@@ -1,1 +1,6 @@
 console.log(10 + 20);
+
+let a = 50;
+let b = 60;
+let c = a + b;
+console.log(c);
