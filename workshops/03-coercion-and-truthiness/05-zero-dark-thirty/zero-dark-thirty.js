761a6289897421f6d3a33c8de7a1c750bdd36821
aa-r-ju <aarju3447@gmail.com>
2025-04-25 20:58:07 +0545

    feat: completed fullstack-hq

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(num) {
+  if (num === 0) {
+    return NaN;
+  }
+
+  let result;
+  let val = "";
+
+  let number = String(num);
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] !== "0") {
+      val += number[i];
+    }
+  }
+  result = Number(val);
+
+  return result;
+}
+
+console.log(zeroDarkThirty(102302));
+console.log(zeroDarkThirty(606.203));
+console.log(zeroDarkThirty(0));
--- workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,19 +1,30 @@
 // Edit the code below
-let place = 'Planet Earth';
+let place = "Planet Earth";
 
-function fullstackHQ(){
-  return middleFunction('United States');
+function fullstackHQ() {
+  return middleFunction("United States");
 
-  function middleFunction(place) {
-    return innerFunction('New York State');
+  function middleFunction(country) {
+    return innerFunction("New York State");
 
-    function innerFunction(place) {
-      place = 'New York City';
+    function innerFunction(state) {
+      let city = "New York City";
       return innermostFunction();
 
       function innermostFunction() {
-        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+        return (
+          "Fullstack HQ is at " +
+          place +
+          ", " +
+          country +
+          ", " +
+          state +
+          ", " +
+          city
+        );
       }
     }
   }
 }
+
+console.log(fullstackHQ());
