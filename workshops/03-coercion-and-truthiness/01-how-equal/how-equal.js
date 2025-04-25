f063697675c4b22cf1276ea5a9e755c560f192e7
aa-r-ju <aarju3447@gmail.com>
2025-04-25 11:33:05 +0545

    feat: completed how-equal of 03-coercion-and-truthyness

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function howEqual(arg1, arg2) {
+  let result;
+  if (arg1 === arg2) {
+    return (result = "strictly");
+  } else if (arg1 == arg2) {
+    return (result = "loosely");
+  } else {
+    return (result = "not equal");
+  }
+  return result;
+}
+
+console.log(howEqual(0, "0"));
+console.log(howEqual(3, 9 / 3));
+console.log(howEqual(true, "truthy"));
