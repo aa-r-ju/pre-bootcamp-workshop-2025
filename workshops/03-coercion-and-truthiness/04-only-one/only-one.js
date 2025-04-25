bbdbc2da2589bd56ef757b15e8eb43c2fe91dfd3
aa-r-ju <aarju3447@gmail.com>
2025-04-25 13:16:26 +0545

    feat: completed only-one

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function onlyOne(val1, val2, val3) {
+  let result = 0;
+  if (val1) {
+    result += 1;
+  } else {
+    result += 0;
+  }
+  if (val2) {
+    result += 1;
+  } else {
+    result += 0;
+  }
+  if (val3) {
+    result += 1;
+  } else {
+    result += 0;
+  }
+  return result === 1;
+}
+
+console.log(onlyOne(false, false, true));
+console.log(onlyOne(0, 1, 2));
