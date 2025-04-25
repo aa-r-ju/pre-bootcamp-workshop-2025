41660d2b926009af5de9f0501a6d96f631ad9cd3
aa-r-ju <aarju3447@gmail.com>
2025-04-25 13:04:15 +0545

    feat: completed my-or-my-and

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function myOr(a, b, c) {
+  if (a) return a;
+  if (b) return b;
+  return c;
+}
+
+console.log(myOr(true, true, false)); // => true
+console.log(myOr(false, null, 0)); // => 0
+
+function myAnd(a, b, c) {
+  if (!a) return a;
+  if (!b) return b;
+  return c;
+}
+
+console.log(myAnd(1, "apples", true)); // => true
+console.log(myAnd(1, null, true)); // => null
