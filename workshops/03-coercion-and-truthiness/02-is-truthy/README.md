eb9ee194f5566dfc59cd798d9c49317f0a45f9ea
aa-r-ju <aarju3447@gmail.com>
2025-04-25 12:37:15 +0545

    feat: completed is-truthy

--- workshops/03-coercion-and-truthiness/02-is-truthy/README.md
+++ workshops/03-coercion-and-truthiness/02-is-truthy/README.md
@@ -16,6 +16,20 @@ the type of the value tested.
 ```
 
 ```javascript
-isTruthy('I yearn for truth'); // => true
+isTruthy("I yearn for truth"); // => true
 isTruthy(null); // => The null value is falsey
 ```
+
+function isTruthy(para) {
+let allFalsyValues = {
+"false": "The boolean value false is falsey",
+"null": "The null value is falsey",
+"undefined": "undefined is falsey",
+"0": "The number 0 is falsey (the only falsey number)",
+"": "The empty string is falsey (the only falsey string)",
+};
+return para ? true : console.log(allFalsyValues[String(para)]);
+}
+
+console.log(isTruthy("I yearn for truth")); // true
+isTruthy(null); // The null value is falsey
--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,28 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// function isTruthy(para) {
+//   if (para === false) {
+//     return "The boolean value false is falsey";
+//   } else if (para === null) {
+//     return "The null value is falsey";
+//   } else if (para === undefined) {
+//     return "undefined is falsey";
+//   } else if (para === 0) {
+//     return "The number 0 is falsey (the only falsey number)";
+//   } else if (para === "") {
+//     return "The empty string is falsey (the only falsey string)";
+//   } else {
+//     return true;
+//   }
+// }
+// console.log(isTruthy("I yearn for truth"));
+// console.log(isTruthy(null));
+function isTruthy(para) {
+  let allFalsyValues = {
+    false: "The boolean value false is falsey",
+    null: "The null value is falsey",
+    undefined: "undefined is falsey",
+    0: "The number 0 is falsey (the only falsey number)",
+    "": "The empty string is falsey (the only falsey string)",
+  };
+  return para ? true : allFalsyValues[String(para)];
+}
