477c52421ffe027a70ceb53532afe0ddfa133f56
aa-r-ju <aarju3447@gmail.com>
2025-04-26 22:52:26 +0545

    feat: completed my-last-index

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function myLastIndexOf(arr, searchValue, startIdx) {
+  let i = startIdx !== undefined ? startIdx : arr.length - 1;
+
+  for (i; i >= 0; i--) {
+    if (arr[i] === searchValue) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(myLastIndexOf(["gee", "gee", "gee", "gee", "baby", "baby"], "gee"));
+console.log(myLastIndexOf(["Tiffany", "Sunny", "Yoona"], "Jessica"));
+console.log(
+  myLastIndexOf(["the", "girls", "bring", "the", "boys", "out"], "the", 2)
+);
