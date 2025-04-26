903c585b2489abfa72262de0514549b1a4b73691
aa-r-ju <aarju3447@gmail.com>
2025-04-26 22:23:21 +0545

    feat: completed my-includes

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function myIncludes(arr, searchvalue) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i], "kkk");
+    if (arr[i] === searchvalue) {
+      return true;
+    }
+  }
+  return false;
+}
+myIncludes([10, 20, 30], 20);
+myIncludes(["apples", "bananas", "citrus"], "pears");
