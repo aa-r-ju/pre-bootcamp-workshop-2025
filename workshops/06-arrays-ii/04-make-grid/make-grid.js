85d8661b2ddecc3c3fc57bc889f7723385461856
aa-r-ju <aarju3447@gmail.com>
2025-04-27 21:36:16 +0545

    feat: completed make-grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function makeGrid(numColumns, numRows) {
+  let bigArr = [];
+  let arr = [];
+  for (let i = 1; i <= numColumns; i++) {
+    arr.push(i);
+  }
+  for (let j = 1; j <= numRows; j++) {
+    bigArr.push(arr.slice());
+  }
+  return bigArr;
+}
+
+console.log(makeGrid(3, 4));
