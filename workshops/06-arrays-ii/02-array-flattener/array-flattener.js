82c45a04e7087fa6713fd199d020dccf369dc8f3
aa-r-ju <aarju3447@gmail.com>
2025-04-27 19:01:04 +0545

    feat: completed array-flattener

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function arrayFlattener(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(arrayFlattener([1, [2, 3], 4]));
