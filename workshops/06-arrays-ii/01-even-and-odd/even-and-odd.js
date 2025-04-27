10e71cae7c0dbc84cf633988a1f292962e821778
aa-r-ju <aarju3447@gmail.com>
2025-04-27 18:53:47 +0545

    feat: completed even-and-odd

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function evenAndOdd(arr) {
+  let neArr = [];
+  let oddNUms = [];
+  let evenNums = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      oddNUms.push(arr[i]);
+    } else {
+      evenNums.push(arr[i]);
+    }
+  }
+  neArr.push(evenNums, oddNUms);
+  return neArr;
+}
+
+console.log(evenAndOdd([1, 2, 3, 4, 5, 6]));
