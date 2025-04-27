3aa3cdbd1d8e865db68176d1f79c50b955cba1b1
aa-r-ju <aarju3447@gmail.com>
2025-04-27 20:39:41 +0545

    feat: completed zero-inventory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+let myZoo = [
+  ["King Kong", ["gorilla", 42]],
+  ["Nemo", ["fish", 5]],
+  ["Punxsutawney Phil", ["groundhog", 11]],
+];
+function zooInventory(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let animalName = arr[i][0];
+    let animalType = arr[i][1][0];
+    let animalAge = arr[i][1][1];
+
+    result.push(`${animalName} the ${animalType} is ${animalAge}.`);
+  }
+  return result;
+}
+
+console.log(zooInventory(myZoo));
