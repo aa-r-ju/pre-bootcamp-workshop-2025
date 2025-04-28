0e4a492efeeca7f3fa8dac2daf9b4947652e3f09
aa-r-ju <aarju3447@gmail.com>
2025-04-28 10:40:53 +0545

    feat: completed my-slice

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function mySlice(originalArray, startIdx = 0, endIdx = originalArray.length) {
+  let result = [];
+
+  if (startIdx < 0) {
+    startIdx = originalArray.length + startIdx;
+  }
+
+  if (endIdx < 0) {
+    endIdx = originalArray.length + endIdx;
+  }
+
+  for (let i = startIdx; i < endIdx; i++) {
+    result.push(originalArray[i]);
+  }
+
+  return result;
+}
