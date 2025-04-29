ea9e683d097e6d9d8f5944ddff03235456b75dff
aa-r-ju <aarju3447@gmail.com>
2025-04-29 14:13:31 +0545

    feat: completed pet-sound

--- workshops/07-objects/04-pet-sounds/pet-sounds.js
+++ workshops/07-objects/04-pet-sounds/pet-sounds.js
@@ -1,40 +1,60 @@
 let animalNoises = [
-  { 'dog': {
-    'America' : 'Woof! Woof!',
-    'Germany' : 'Wau Wau!',
-    'England' : 'Bow wow!',
-    'Uruguay' : 'Jua jua!',
-    'Afrikaans' : 'Blaf!',
-    'Korea' : 'Mong mong!',
-    'Iceland' : 'Voff voff!',
-    'Albania' : 'Ham!',
-    'Algeria' : 'Ouaf ouaf!'
-    }
+  {
+    dog: {
+      America: "Woof! Woof!",
+      Germany: "Wau Wau!",
+      England: "Bow wow!",
+      Uruguay: "Jua jua!",
+      Afrikaans: "Blaf!",
+      Korea: "Mong mong!",
+      Iceland: "Voff voff!",
+      Albania: "Ham!",
+      Algeria: "Ouaf ouaf!",
+    },
   },
-  { 'cat': {
-    'America' : 'Meow',
-    'Germany' : 'Miauw!',
-    'England' : 'mew mew',
-    'Uruguay' : 'Miau Miau!',
-    'Afrikaans' : 'Purr',
-    'Korea' : 'Nyaong!',
-    'Iceland' : 'Kurnau!',
-    'Albania' : 'Miau',
-    'Algeria' : 'Miaou!'
-    }
+  {
+    cat: {
+      America: "Meow",
+      Germany: "Miauw!",
+      England: "mew mew",
+      Uruguay: "Miau Miau!",
+      Afrikaans: "Purr",
+      Korea: "Nyaong!",
+      Iceland: "Kurnau!",
+      Albania: "Miau",
+      Algeria: "Miaou!",
+    },
   },
-  { 'chicken': {
-    'America' : 'Cluck cluck',
-    'Germany' : 'tock tock tock',
-    'England' : 'Cluck Cluck',
-    'Uruguay' : 'gut gut gdak',
-    'Afrikaans' : 'kukeleku',
-    'Korea' : 'ko-ko-ko',
-    'Iceland' : 'Chuck-chuck!',
-    'Albania' : 'Kotkot',
-    'Algeria' : 'Cotcotcodet'
-    }
-  }
+  {
+    chicken: {
+      America: "Cluck cluck",
+      Germany: "tock tock tock",
+      England: "Cluck Cluck",
+      Uruguay: "gut gut gdak",
+      Afrikaans: "kukeleku",
+      Korea: "ko-ko-ko",
+      Iceland: "Chuck-chuck!",
+      Albania: "Kotkot",
+      Algeria: "Cotcotcodet",
+    },
+  },
 ];
 
 // YOUR CODE BELOW
+function petSounds(animal, country) {
+  for (let i = 0; i < animalNoises.length; i++) {
+    let bal = animalNoises[i];
+    let name = bal[animal];
+
+    if (name) {
+      let sound = name[country];
+
+      let animalPlural = animal[0].toUpperCase() + animal.slice(1) + "s";
+
+      return `${animalPlural} in ${country} say ${sound}`;
+    }
+  }
+  return "Animal or country not found.";
+}
+
+console.log(petSounds("dog", "Iceland"));
