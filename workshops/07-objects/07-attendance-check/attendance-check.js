2b55d253ec967ac21de2d83fc6ea066754056854
aa-r-ju <aarju3447@gmail.com>
2025-04-29 15:06:00 +0545

    feat: completed attendance-check

--- workshops/07-objects/07-attendance-check/attendance-check.js
+++ workshops/07-objects/07-attendance-check/attendance-check.js
@@ -1,40 +1,61 @@
 let classRoom = [
-    {
-        "Marnie" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : true},
-                {"Thursday" : true},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Lena" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : true},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    },
-    {
-        "Shoshanna" : [
-                {"Monday" : true},
-                {"Tuesday" : true},
-                {"Wednesday" : false},
-                {"Thursday" : true},
-                {"Friday" : false}
-            ]
-    },
-    {
-        "Jessa" : [
-                {"Monday" : false},
-                {"Tuesday" : false},
-                {"Wednesday" : false},
-                {"Thursday" : false},
-                {"Friday" : true}
-            ]
-    }
+  {
+    Marnie: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: true },
+      { Thursday: true },
+      { Friday: true },
+    ],
+  },
+  {
+    Lena: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: true },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
+  {
+    Shoshanna: [
+      { Monday: true },
+      { Tuesday: true },
+      { Wednesday: false },
+      { Thursday: true },
+      { Friday: false },
+    ],
+  },
+  {
+    Jessa: [
+      { Monday: false },
+      { Tuesday: false },
+      { Wednesday: false },
+      { Thursday: false },
+      { Friday: true },
+    ],
+  },
 ];
 
-// YOUR CODE BELOW
+function attendanceCheck(day) {
+  let presentStudents = [];
+
+  for (let i = 0; i < classRoom.length; i++) {
+    let studentObj = classRoom[i];
+    let studentName = Object.keys(studentObj)[0];
+    let attendanceArray = studentObj[studentName];
+
+    for (let j = 0; j < attendanceArray.length; j++) {
+      let dayObj = attendanceArray[j];
+
+      if (dayObj[day] === true) {
+        presentStudents.push(studentName);
+        break;
+      }
+    }
+  }
+
+  return presentStudents;
+}
+
+console.log(attendanceCheck("Monday"));
