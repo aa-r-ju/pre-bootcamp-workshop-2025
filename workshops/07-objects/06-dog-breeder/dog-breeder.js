3a3ccf9ec3155877f69891dea998b81ad834efc8
aa-r-ju <aarju3447@gmail.com>
2025-04-29 14:46:45 +0545

    feat: completed dog-breeder.js

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function dogBreeder(name = "Steve", age = 0) {
+  if (typeof name === "number") {
+    age = name;
+    name = "Steve";
+  }
+  return {
+    name: name,
+    age: age,
+  };
+}
+console.log(dogBreeder("Sam", 12));
+console.log(dogBreeder(15));
+console.log(dogBreeder());
