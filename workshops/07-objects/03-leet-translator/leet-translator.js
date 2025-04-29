22b05185fa379b326b334916e2d9ed9d9994a83e
aa-r-ju <aarju3447@gmail.com>
2025-04-29 12:31:41 +0545

    feat: completed leet-translator

--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -1,4 +1,84 @@
-let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let leetChars = [
+  "@",
+  "8",
+  "(",
+  "|)",
+  "3",
+  "ph",
+  "g",
+  "#",
+  "l",
+  "_|",
+  "|<",
+  "1",
+  "|'|'|",
+  "//",
+  "0",
+  "|D",
+  "(,)",
+  "|2",
+  "5",
+  "+",
+  "|_|",
+  "|/",
+  "|/|/'",
+  "><",
+  "j",
+  "2",
+];
 
 // YOUR CODE BELOW
+function leetTranslator(str) {
+  let result = "";
+
+  let obj = {};
+
+  for (let i = 0; i < letters.length; i++) {
+    let letter = letters[i];
+    let leet = leetChars[i];
+    obj[letter] = leet;
+  }
+
+  str = str.toLowerCase();
+  for (let j = 0; j < str.length; j++) {
+    let currentChart = str[j];
+
+    if (obj[currentChart]) {
+      result += obj[currentChart];
+    } else {
+      result += currentChart;
+    }
+  }
+  return result;
+}
+
+console.log(leetTranslator("Fullstack"));
+console.log(leetTranslator("Aarju"));
