bd49cd0fd68c5795e76c47de20f35e84d60249c5
aa-r-ju <aarju3447@gmail.com>
2025-04-29 11:38:26 +0545

    feat: completed last-friday-night

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,30 @@
 // YOUR CODE BELOW
+
+let transactions = [
+  {
+    name: "Tons of glitter",
+    amount: 70,
+  },
+  {
+    name: "Porcelain Pink Flamingos",
+    amount: 92,
+  },
+  {
+    name: "Chandelier replacement",
+    amount: 10000,
+  },
+  {
+    name: "Dinner at TGIF x6",
+    amount: 350,
+  },
+];
+
+function lastFridayNight(para) {
+  let totalAmount = 0;
+  for (let i = 0; i < para.length; i++) {
+    totalAmount += para[i].amount;
+  }
+  return totalAmount;
+}
+
+console.log(lastFridayNight(transactions));
