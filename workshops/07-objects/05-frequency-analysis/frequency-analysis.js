4f65878afde3ef26afda547c1612be6251f9086f
aa-r-ju <aarju3447@gmail.com>
2025-04-29 14:33:17 +0545

    feat: completed frequency-analysis

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(str) {
+  let obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (obj[char]) {
+      obj[char] += 1;
+    } else {
+      obj[char] = 1;
+    }
+  }
+  return obj;
+}
+
+console.log(frequencyAnalysis("abcabbbb"));
