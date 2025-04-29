af75e0415784cad5ae556ca51323f08d3a584f75
aa-r-ju <aarju3447@gmail.com>
2025-04-29 17:55:30 +0545

    feat: completed say-your-name

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+let me2 = {
+  name: "Tarana",
+  getGreeting: function (para) {
+    return `Hi ${para.name}, my name is ${me2.name}. `;
+  },
+};
+
+let you = {
+  name: "Alyssa",
+};
+
+console.log(me2.getGreeting(you));
