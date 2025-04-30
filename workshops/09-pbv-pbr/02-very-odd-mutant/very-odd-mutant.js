b2872d6ca5b1b66fb5a58232459800d7ef58bfdb
aa-r-ju <aarju3447@gmail.com>
2025-04-30 13:44:35 +0545

    feat: completed very-odd-mutant

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function veryOddMutant(arr) {
+  let countRemoved = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr[i] = "normie";
+      countRemoved++;
+    }
+  }
+  return countRemoved;
+}
+let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(veryOddMutant(allTheNums));
+console.log(allTheNums);
