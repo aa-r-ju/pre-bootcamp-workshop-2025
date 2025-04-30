bb7e4a2a3751ef71fc9dc228438686bf51497ccc
aa-r-ju <aarju3447@gmail.com>
2025-04-30 13:26:48 +0545

    feat: completed very-odd.js

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function veryOdd(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
