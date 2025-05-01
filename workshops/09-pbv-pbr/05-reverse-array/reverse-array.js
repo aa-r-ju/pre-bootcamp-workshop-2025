d21046fdd8815e5eba03c68ad7ed8a419e2ad856
aa-r-ju <aarju3447@gmail.com>
2025-05-01 14:59:03 +0545

    feat: completed reverse-array

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,15 +1,14 @@
 // YOUR CODE BELOW
-function reverse(arr) {
-  let left = 0;
-  let right = arr.length - 1;
-
-  while (left < right) {
-    // Swap the elements at left and right indices
+function reverseArray(arr) {
+  for (let left = 0, right = arr.length - 1; left < right; left++, right--) {
     let temp = arr[left];
     arr[left] = arr[right];
     arr[right] = temp;
-
-    left++;
-    right--;
   }
+
+  return arr;
 }
+
+let myArray = [1, 2, 3, 4];
+let reversedArray = reverseArray(myArray);
+console.log(reversedArray);
