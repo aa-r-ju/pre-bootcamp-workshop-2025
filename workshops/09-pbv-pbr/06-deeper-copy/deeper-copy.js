88a1aac41d85b8ed6307b7f2b291529a10015c4d
aa-r-ju <aarju3447@gmail.com>
2025-05-01 15:01:53 +0545

    feat: completed deeper-copy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function deeperCopy(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArr.push(deeperCopy(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
