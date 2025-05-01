978fa02b42a99cea209fddda21bdc36fb6e5b7e3
aa-r-ju <aarju3447@gmail.com>
2025-05-02 00:21:14 +0545

    feat: completed chain-reaction.js

--- workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function chainReaction(value, array) {
+  let total = value;
+  for (let i = 0; i < array.length; i++) {
+    total = array[i](total);
+  }
+  return total;
+}
+
+function addTen(num) {
+  return num + 10;
+}
+
+function subtractFive(num) {
+  return num - 5;
+}
+
+function multiplyFive(num) {
+  return num * 5;
+}
+
+console.log(chainReaction(0, [addTen, subtractFive, multiplyFive])); // => 25
+chainReaction(0, [subtractFive, multiplyFive, addTen]); // => -15
