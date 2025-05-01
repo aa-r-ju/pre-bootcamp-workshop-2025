89a6965678cf41df33a5945ccade4c3058ea8ccb
aa-r-ju <aarju3447@gmail.com>
2025-05-01 21:36:15 +0545

    feat: completed my-for-each.js

--- workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function myForEach(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
+}
+
+let sum = 0;
+
+function addToSum(num) {
+  sum += num;
+}
+
+let nums = [1, 2, 3];
+
+myForEach(nums, addToSum);
+
+console.log(sum); // 6
