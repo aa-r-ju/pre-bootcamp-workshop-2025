d902f93096022e252c0e98f809118bacb97282a3
aa-r-ju <aarju3447@gmail.com>
2025-05-01 20:53:00 +0545

    feat: completed function-logger.js

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function functionLogger(fun, val) {
+  console.log("Function starting");
+  let result = fun(val);
+  console.log("Function complete");
+  return result;
+}
+
+function squareNum(x) {
+  return x * x;
+}
+let squareOfFour = functionLogger(squareNum, 4);
+
+console.log(squareOfFour);
