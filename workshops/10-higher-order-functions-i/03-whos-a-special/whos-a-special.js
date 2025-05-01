e4259b6943e876f6b20b2944ba01a1ae9f6cbb4a
aa-r-ju <aarju3447@gmail.com>
2025-05-01 21:20:07 +0545

    feat: completed whos-a-special.js

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+function whosASpecial(arr) {
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result += `${arr[i].name} the ${arr[i].species} is very special! `;
+  }
+  return result.trim();
+}
+
+let specialPets = [
+  {
+    name: "Sadie",
+    species: "cat",
+  },
+  {
+    name: "Layla",
+    species: "cat",
+  },
+  {
+    name: "Bogie",
+    species: "dog",
+  },
+];
+
+console.log(whosASpecial(specialPets));
