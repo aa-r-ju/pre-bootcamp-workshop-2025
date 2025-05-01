14f794d9d8a26e5b951ae92abe7b55cf25fe9dd9
aa-r-ju <aarju3447@gmail.com>
2025-05-01 14:37:05 +0545

    feat: completed deeper-copy.js

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function reverse(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    // Swap the elements at left and right indices
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+
+    left++;
+    right--;
+  }
+}
