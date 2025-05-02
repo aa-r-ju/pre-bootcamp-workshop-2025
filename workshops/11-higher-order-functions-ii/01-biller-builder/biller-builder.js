9a8448e8b303bbcb32642c311cbe880cd67714d2
aa-r-ju <aarju3447@gmail.com>
2025-05-02 10:06:43 +0545

    feat: completed buller-builder.js

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function billerBuilder(state) {
+  return function (price) {
+    if (state === "NY") {
+      return price * 1.03 * 1.04;
+    } else if (state === "NJ") {
+      return price * 1.05 * 1.06625;
+    } else {
+      throw new Error("Unsupported state");
+    }
+  };
+}
+
+let newYorkBiller = billerBuilder("NY");
+console.log(newYorkBiller(100)); // => 107.12 (100 * 1.03 * 1.04)
+
+let newJersBiller = billerBuilder("NJ");
+console.log(newJersBiller(100)); // => 111.95625 (100 * 1.05 * 1.06625)
