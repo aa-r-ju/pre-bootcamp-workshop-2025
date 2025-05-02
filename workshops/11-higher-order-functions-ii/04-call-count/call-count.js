9b4ebc901ea20259386a32aa5e7570afc803f91f
aa-r-ju <aarju3447@gmail.com>
2025-05-02 10:38:17 +0545

    feat: call-count.js

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function callCount() {
+  let val = 0;
+
+  return function () {
+    val++;
+    return val;
+  };
+}
+let newFunction1 = callCount();
+let newFunction2 = callCount();
+
+newFunction1(); // => 1
+newFunction1(); // => 2
+
+newFunction2(); // => 1
+newFunction2(); // => 2
