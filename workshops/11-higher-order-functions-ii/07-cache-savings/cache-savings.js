b5dba71ed647b3fa664f919d684f81863959ffaf
aa-r-ju <aarju3447@gmail.com>
2025-05-02 11:18:15 +0545

    feat: completed cache-saving.js

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  const cache = {};
+
+  return function (arg) {
+    if (arg in cache) {
+      return cache[arg];
+    } else {
+      const result = callback(arg);
+      cache[arg] = result;
+      return result;
+    }
+  };
+}
