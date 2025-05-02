6a107a053ddba19c37adf8652b27c341be7e9f67
aa-r-ju <aarju3447@gmail.com>
2025-05-02 10:10:10 +0545

    feat: completed times-table

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function timesTable(num) {
+  return function (val) {
+    return num * val;
+  };
+}
+
+let ninesTable = timesTable(9);
+console.log(ninesTable(8)); // => 72
+
+let twelvesTable = timesTable(12);
+console.log(twelvesTable(100)); // => 1200
