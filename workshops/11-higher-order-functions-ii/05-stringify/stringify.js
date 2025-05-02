03d4a842af90c6f14c2762473bbe59524387edf5
aa-r-ju <aarju3447@gmail.com>
2025-05-02 10:45:46 +0545

    feat: completed stringify.js

--- workshops/11-higher-order-functions-ii/05-stringify/stringify.js
+++ workshops/11-higher-order-functions-ii/05-stringify/stringify.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function stringify(callback) {
+  return function () {
+    let val = String(callback());
+    return val;
+  };
+}
+
+function returnsANumber() {
+  return 100;
+}
+
+let newFunction = stringify(returnsANumber);
+console.log(newFunction()); // => '100'
