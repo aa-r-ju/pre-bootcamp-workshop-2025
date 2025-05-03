04983872959a05a10b065994a4fb112c20c58cb9
aa-r-ju <aarju3447@gmail.com>
2025-05-03 22:12:17 +0545

    feat: completed count-vowels

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function countVowels(str) {
+  if (str.length === 0) return 0;
+
+  const vowels = "aeiouAEIOU";
+  const firstChar = str[0];
+  const isVowel = vowels.includes(firstChar) ? 1 : 0;
+
+  return isVowel + countVowels(str.slice(1));
+}
