cfb0441acca4b3d8c519d173ae78543150063af5
aa-r-ju <aarju3447@gmail.com>
2025-05-04 12:37:51 +0545

    feat: completed sum-digits

--- workshops/12-recursion-i/06-sum-digits/sum-digits.js
+++ workshops/12-recursion-i/06-sum-digits/sum-digits.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function sumDigits(num) {
+  const str = String(num);
+  if (str.length === 1) {
+    return Number(str);
+  }
+  return Number(str[str.length - 1]) + sumDigits(str.slice(0, -1));
+}
+
+// Example:
+console.log(sumDigits(1234));
