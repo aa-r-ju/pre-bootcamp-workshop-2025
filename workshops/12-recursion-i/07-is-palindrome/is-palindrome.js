8b6fe53beb74e43c4471fe453cd789ae6c9d02f9
aa-r-ju <aarju3447@gmail.com>
2025-05-04 12:41:40 +0545

    feat: completed is-palindrome.js

--- workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function isPalindrome(str) {
+  str = str.toLowerCase();
+
+  if (str.length <= 1) {
+    return true;
+  }
+
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+
+  return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("Kayak"));
+console.log(isPalindrome("NEVERODDOREVEN"));
+console.log(isPalindrome("Tacocat"));
+console.log(isPalindrome("SELFLESS"));
