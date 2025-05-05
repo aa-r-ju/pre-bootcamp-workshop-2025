b586afb60d2eb5823a0744bf986818c24172287b
aa-r-ju <aarju3447@gmail.com>
2025-05-05 19:22:34 +0545

    feat: completed all-sysytems-go

--- workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
+++ workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function allSystemsGo(system) {
+  for (let key in system) {
+    const value = system[key];
+
+    if (typeof value === "object") {
+      if (!allSystemsGo(value)) return false;
+    } else if (value !== true) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(allSystemsGo(systems));
