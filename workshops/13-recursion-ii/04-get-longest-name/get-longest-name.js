09d63abdcb939334d64ce3dbe67e64ca33abaaf5
aa-r-ju <aarju3447@gmail.com>
2025-05-05 21:25:27 +0545

    feat: completed get-longest-name

--- workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
+++ workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+
+function getLongestName(family) {
+  let longest = "";
+
+  for (let name in family) {
+    let childLongest = getLongestName(family[name] || {}); // safe recursive call
+    if (name.length > longest.length) longest = name;
+    if (childLongest.length > longest.length) longest = childLongest;
+  }
+
+  return longest;
+}
+
+console.log(getLongestName(family));
+// => 'Sir Paddington the Fourth, of the county Wilstonshire'
+
+let family = {
+  "Beverly Marquez": {
+    "Nina Rhone": {
+      "William Rhodes": null,
+      "Paul Nell": null,
+      "Sir Paddington the Fourth, of the county Wilstonshire": null,
+    },
+  },
+};
+
+getLongestName(family); // => 'Sir Paddington the Fourth, of the county Wilstonshire'
