d53f22beb3ade6fae807a872e6b3f9acbf6c6486
aa-r-ju <aarju3447@gmail.com>
2025-05-05 21:28:11 +0545

    feat: completed search-party

--- workshops/13-recursion-ii/05-search-party/search-party.js
+++ workshops/13-recursion-ii/05-search-party/search-party.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function searchParty(name, world) {
+  for (let key in world) {
+    const value = world[key];
+
+    if (typeof value === "string") {
+      if (value === name) {
+        return [key];
+      }
+    } else if (Array.isArray(value)) {
+      if (value.includes(name)) {
+        return [key];
+      }
+    } else if (typeof value === "object" && value !== null) {
+      const path = searchParty(name, value);
+      if (path) {
+        return [key, ...path];
+      }
+    }
+  }
+
+  return null;
+}
