181c754482851e79e482590c433f9a1423e978c4
aa-r-ju <aarju3447@gmail.com>
2025-05-05 19:57:10 +0545

    feat: completed the-truth-counts

--- workshops/13-recursion-ii/03-the-truth-counts/the-truth-counts.js
+++ workshops/13-recursion-ii/03-the-truth-counts/the-truth-counts.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function theTruthCounts(arr) {
+  let count = 0;
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      count += theTruthCounts(item);
+    } else if (item) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+console.log(theTruthCounts([0, [true, ["yes"]]])); // => 2
